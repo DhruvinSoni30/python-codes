79a89d09d92f36fd5ab4d82164cc5d48e05c2924
Dhruvin Soni <37222087+DhruvinSoni30@users.noreply.github.com>
2021-07-12 14:32:49 +0530

    Update condition.py

--- condition.py
+++ condition.py
@@ -3,9 +3,6 @@ name_of_unit = "hours"
 
 
 def days_to_units(num_of_days):
-    condition_check = num_of_days > 0
-    print(type(condition_check))
-    print(condition_check)
     if num_of_days > 0:
         return f"{num_of_days} days are {num_of_days * calculation_to_unit} {name_of_unit}"
     elif num_of_days == 0:
@@ -14,11 +11,16 @@ def days_to_units(num_of_days):
         return "Invalid input, Please enter valid input!"
 
 
+def validate_and_execute():
+    if user_input.isdigit():
+        user_input_number = int(user_input)
+        calculated_value = days_to_units(user_input_number)
+        print(calculated_value)
+    else:
+        print("Invalid input, Please enter a valid number!")
+
+
 user_input = input("Enter the number in days & I will convert it into hours!\n")
 
-if user_input.isdigit():
-    user_input_number = int(user_input)
-    calculated_value = days_to_units(user_input_number)
-    print(calculated_value)
-else:
-    print("Invalid input, Please enter a valid number!")
+validate_and_execute()
+
