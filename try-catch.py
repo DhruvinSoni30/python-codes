bd03d1105c75038295e506253da71dbc2d182c0e
Dhruvin Soni <37222087+DhruvinSoni30@users.noreply.github.com>
2021-07-13 01:26:24 +0530

    Create try-catch.py

---
+++ try-catch.py
@@ -0,0 +1,24 @@
+calculation_to_unit = 24
+name_of_unit = "hours"
+
+
+def days_to_units(num_of_days):
+    return f"{num_of_days} days are {num_of_days * calculation_to_unit} {name_of_unit}"
+
+
+def validate_and_execute():
+    try:
+        user_input_number = int(user_input)
+        if user_input_number > 0:
+            calculated_value = days_to_units(user_input_number)
+            print(calculated_value)
+        elif user_input_number == 0:
+            print("You entered 0, please enter valid positive number!")
+    except ValueError:
+        print("Invalid input, Please enter a valid number!")
+
+
+user_input = input("Enter the number in days & I will convert it into hours!\n")
+
+validate_and_execute()
+
